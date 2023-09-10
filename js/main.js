99baca5459e4c3cadc8f6ec5d27a9176c9060831
Fabiano <fabiano.ssantos79@gmail.com>
2023-09-10 13:10:42 -0300

    update system

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const form = document.querySelector('#novoItem')
+const lista = document.querySelector('#lista')
 
 form.addEventListener('submit', (item) => {
     item.preventDefault();
     const nome = item.target.elements['nome'].value
     const qtde = item.target.elements['quantidade'].value
-    console.log(nome, qtde);
+
+    criarElemento(nome, qtde)
 })
+
+function criarElemento(nome, quantidade) {
+    const li = document.createElement('li')
+    li.classList.add('item')
+
+    const strong = document.createElement('strong')
+    strong.innerHTML = quantidade
+
+    li.appendChild(strong)
+    li.innerHTML += nome
+
+    lista.appendChild(li)
+}
